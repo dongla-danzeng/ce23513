ea2187ab3ed84061d69f21e08632ae0c8a14c2e4
ihjycc <80345055+ihjycc@users.noreply.github.com>
2023-04-13 00:13:51 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -169,9 +169,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>GPT_ihjycc</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            黄甲岩的机器人.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
